65a8b77f67185f0694efe2deea57748c539f0c80
Ken Reidy <kenreidy@users.noreply.github.com>
2015-03-09 05:16:12 +0700

    Create script.js
    Copy content of this js file to your js file.
    
    It also possible to paste content of this file to Genesis Footer Scripts, but you need to <script> tag before the code and close it with </script> after the last code.

---
+++ script.js
@@ -0,0 +1,8 @@
+jQuery('ul.genesis-nav-menu.menu-primary').slimmenu({
+    resizeWidth: '800',
+    collapserTitle: 'Main Menu',
+    easingEffect:'easeInOutQuint',
+    animSpeed:'medium',
+    indentChildren: true,
+    childrenIndenter: '&raquo;'
+});
